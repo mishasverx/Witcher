30ff44a9b62d6ac5813d30a2fccda26814106e01
mishasverx <mishasverx@gmail.com>
2021-12-11 18:03:32 +0300

    player file and main

--- player.py
+++ player.py
@@ -1,20 +1,9 @@
 import pygame as pg
+import main
+walk = [pg.image.load("source/player/walk.1.png"), pg.image.load("source/player/walk.2.png"),
+        pg.image.load("source/player/walk.3.png")]
+SPEED = 5
+width = 300
+height = 300
+x, y = main.width // 2, main.height // 2
 
-if __name__ == "__main__":
-    pg.init()
-    pg.display.set_caption("WITCHER")
-    size = width, height = 1600, 800
-    screen = pg.display.set_mode(size)
-
-    running = True
-    FPS = 60
-    x, y = width // 2, height // 2
-    clock = pg.time.Clock()
-
-    while running:
-        for event in pg.event.get():
-            if event.type == pg.QUIT:
-                running = False
-        clock.tick(FPS)
-        pg.display.flip()
-    pg.quit()
