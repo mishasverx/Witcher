b5bf4e921dc24f9e5aed902dcaa2f8f5f8a10c79
mishasverx <mishasverx@gmail.com>
2021-12-11 17:35:34 +0300

    player file

---
+++ player.py
@@ -0,0 +1,19 @@
+import pygame as pg
+
+if __name__ == "__main__":
+    pg.init()
+    pg.display.set_caption("WITCHER")
+    size = width, height = 1600, 800
+    screen = pg.display.set_mode(size)
+
+    running = True
+    FPS = 60
+    clock = pg.time.Clock()
+
+    while running:
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                running = False
+        clock.tick(FPS)
+        pg.display.flip()
+    pg.quit()
