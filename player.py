9b9355875355c7cb50bca6aac536e2d0145e2022
mishasverx <mishasverx@gmail.com>
2021-12-11 19:32:02 +0300

    player file and main

--- player.py
+++ player.py
@@ -1,12 +1,12 @@
 import pygame as pg
 import main
+
 walk_right = [pg.image.load("source/player/right/1.png"), pg.image.load("source/player/right/2.png"),
-        pg.image.load("source/player/right/3.png")]
+              pg.image.load("source/player/right/3.png")]
 walk_left = [pg.image.load("source/player/left/1.png"), pg.image.load("source/player/left/2.png"),
-        pg.image.load("source/player/left/3.png")]
+             pg.image.load("source/player/left/3.png")]
 stay = pg.image.load("source/player/stand/3.png")
 SPEED = 500
 width = 300
 height = 300
 x, y = 0, 0
-
