7aa5ae849f33558f98a36d93d405568d69b99615
mishasverx <mishasverx@gmail.com>
2021-12-11 19:00:14 +0300

    player file and main

--- main.py
+++ main.py
@@ -1,19 +1,54 @@
 import pygame as pg
 import player
+
 if __name__ == "__main__":
     pg.init()
     pg.display.set_caption("WITCHER")
-    size = width, height = 1600, 800
+    width = 1600
+    height = 900
+    x, y = 150, 670
+    size = width, height
     screen = pg.display.set_mode(size)
 
     running = True
     FPS = 60
     clock = pg.time.Clock()
+    left = False
+    right = False
+    anim_count = 0
+    bg = pg.image.load("source/background.png")
+
+
+    def drawPlayer():
+        global anim_count
+        screen.blit(bg, (0, 0))
+        if anim_count + 1 >= 10:
+            anim_count = 0
+        if right:
+            screen.blit(player.walk[anim_count // 3], (x, y))
+            anim_count += 1
+        if left:
+            pass
+        pg.display.update()
 
     while running:
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 running = False
+            keys = pg.key.get_pressed()
+            if keys[pg.K_d] and x > 5:
+                x += player.SPEED / FPS
+                right = True
+                left = False
+            elif keys[pg.K_a] and player.x < 500:
+                player.x -= player.SPEED / FPS
+                left = True
+                right = False
+            else:
+                right = False
+                left = False
+                anim_count = 0
+        drawPlayer()
         clock.tick(FPS)
         pg.display.flip()
     pg.quit()
--- player.py
+++ player.py
@@ -1,9 +1,9 @@
 import pygame as pg
 import main
-walk = [pg.image.load("source/player/walk.1.png"), pg.image.load("source/player/walk.2.png"),
-        pg.image.load("source/player/walk.3.png")]
-SPEED = 5
+walk = [pg.image.load("source/player/walk/1.png"), pg.image.load("source/player/walk/2.png"),
+        pg.image.load("source/player/walk/3.png")]
+SPEED = 300
 width = 300
 height = 300
-x, y = main.width // 2, main.height // 2
+x, y = 0, 0
 
