eb808bd2bcd5278d22218b23fab1558db97c4aca
mishasverx <mishasverx@gmail.com>
2021-12-12 20:02:15 +0300

    удары

--- main.py
+++ main.py
@@ -11,15 +11,18 @@ if __name__ == "__main__":
     screen = pg.display.set_mode(size)
 
     running = True
+    right, left, left_hit, right_hit = False
     FPS = 60
     clock = pg.time.Clock()
     anim_count = 0
+    anim_count2 = 0
     bg = pg.image.load("source/background.png")
 
 
     def drawPlayer():
-        global anim_count
+        global anim_count, anim_count2
         global x, y
+        global right, left
         screen.blit(bg, (0, 0))
         keys = pg.key.get_pressed()
         if keys[pg.K_d] and x < 1350 and y > 50:
@@ -36,8 +39,10 @@ if __name__ == "__main__":
             right = False
             left = False
             anim_count = 0
+
         if anim_count >= 9:
             anim_count = 0
+
         if right:
             screen.blit(player.walk_right[anim_count // 3], (x, y))
             anim_count += 1
@@ -53,6 +58,15 @@ if __name__ == "__main__":
             else:
                 screen.blit(player.stay_left, (x, y))
                 pg.draw.rect(screen, (255, 255, 255), (x, y, 300, 300), 1)
+        for event in pg.event.get():
+            if event.type == pg.MOUSEBUTTONDOWN and event.button == 1 and (right or player.last_dir):
+                right_hit = True
+            if event.type == pg.MOUSEBUTTONDOWN and event.button == 1 and (left or not(player.last_dir)):
+                left_hit = True
+            if anim_count2 > 10:
+                anim_count2 = 0
+            if right_hit:
+
 
         pg.display.update()
 
--- player.py
+++ player.py
@@ -7,6 +7,12 @@ walk_left = [pg.image.load("source/player/left/1.png"), pg.image.load("source/pl
              pg.image.load("source/player/left/3.png")]
 stay_right = pg.image.load("source/player/stand/right.png")
 stay_left = pg.image.load("source/player/stand/left.png")
+right_hit = [pg.image.load("source/player/right_hit/1.png"), pg.image.load("source/player/right_hit/2.png"),
+             pg.image.load("source/player/right_hit/3.png"), pg.image.load("source/player/right_hit/4.png"),
+             pg.image.load("source/player/right_hit/5.png")]
+left_hit = [pg.image.load("source/player/left_hit/1.png"), pg.image.load("source/player/left_hit/2.png"),
+            pg.image.load("source/player/left_hit/3.png"), pg.image.load("source/player/left_hit/4.png"),
+            pg.image.load("source/player/left_hit/5.png")]
 last_dir = True
 SPEED = 10
 width = 300
