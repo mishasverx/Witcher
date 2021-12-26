abaac530fbc130b8077104462f6fc1a1dc742f44
mishasverx <mishasverx@gmail.com>
2021-12-27 01:53:20 +0300

    короче начал там ведьмака самого чуть делал

--- main2WITCHERGAME.py
+++ main2WITCHERGAME.py
@@ -21,22 +21,52 @@ def load_image(name, colorkey=None):
 
 pg.init()
 pg.display.set_caption("WITCHER")
-width = 1600
-height = 900
+width, height = 1600, 900
 size = width, height
 screen = pg.display.set_mode(size)
 all_sprites = pg.sprite.Group()
+witcher_sprites = pg.sprite.Group()
 tile_group = pg.sprite.Group()
 tile_images = {
     'floor': load_image('source\\tile\\floour1.jpg'),
     'plat': load_image('source\\tile\\plat.jpg')
 }
+witcher_images = {
+    "walk_right": [load_image("source/player/right/2.png"), load_image("source/player/right/3.png"),
+                   load_image("source/player/right/2.png"), load_image("source/player/right/1.png")],
+    "walk_left": [load_image("source/player/left/2.png"), load_image("source/player/left/3.png"),
+                  load_image("source/player/left/2.png"), load_image("source/player/left/1.png")],
+    "stand_right": [load_image("source/player/stand_right/1.png"), load_image("source/player/stand_right/2.png"),
+                    load_image("source/player/stand_right/3.png"), load_image("source/player/stand_right/4.png"),
+                    load_image("source/player/stand_right/5.png")],
+    "stand_left": [load_image("source/player/stand_left/1.png"), load_image("source/player/stand_left/2.png"),
+                   load_image("source/player/stand_left/3.png"), load_image("source/player/stand_left/4.png"),
+                   load_image("source/player/stand_left/5.png")]
+}
 floor_width, floor_height = 80, 50
-plat_width, plat_height = 80, 40
+plat_width, plat_height = 80, 50
 running = True
 FPS = 60
+x, y = 100, 100
 clock = pg.time.Clock()
 
+
+class Witcher(pg.sprite.Sprite):
+    def __init__(self, type, x, y):
+        super().__init__(witcher_sprites, all_sprites)
+        self.image = witcher_images[type]
+        self.x, self.y = x, y
+        self.count = 1
+
+    def move(self, pos):
+        if self.count >= 24:
+            self.count = 0
+        if pos == "right" and self.count > 0:
+            self.image = witcher_images["walk_right"][self.count // 6]
+            self.count += 1
+        elif pos == "left" and self.count > 0:
+            self.image = witcher_images["walk_left"][self.count // 6]
+            self.count += 1
 
 class Tile(pg.sprite.Sprite):
     def __init__(self, type, x, y):
@@ -50,15 +80,6 @@ class Tile(pg.sprite.Sprite):
                 plat_width * x, plat_height * y)
 
 
-def load_level(filename):
-    with open(filename, 'r') as mapFile:
-        level_map = [line.strip() for line in mapFile]
-
-    max_width = max(map(len, level_map))
-
-    return list(map(lambda x: list(x.ljust(max_width, '.')), level_map))
-
-
 def generate_level(level):
     x, y, count = None, None, 0
     for y in range(len(level)):
@@ -68,6 +89,13 @@ def generate_level(level):
             elif level[y][x] == "#":
                 Tile('plat', x, y)
     return x, y
+
+
+def load_level(filename):
+    with open(filename, 'r') as mapFile:
+        level_map = [line.strip() for line in mapFile]
+    max_width = max(map(len, level_map))
+    return list(map(lambda x: list(x.ljust(max_width, '.')), level_map))
 
 
 level = load_level('map_1.txt')
@@ -89,8 +117,9 @@ while running:
     for event in pg.event.get():
         if event.type == pg.QUIT:
             running = False
-        all_sprites.draw(screen)
+        # all_sprites.draw(screen)
         tile_group.draw(screen)
+        witcher_sprites.draw(screen)
         clock.tick(FPS)
         pg.display.flip()
 pg.quit()
