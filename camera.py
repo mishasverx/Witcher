c6a5c76369308f02e4c3c99f2dce155ac1cfcd58
mishasverx <mishasverx@gmail.com>
2022-02-23 22:54:55 +0300

    cam

--- camera.py
+++ camera.py
@@ -7,11 +7,11 @@ class Camera:
         if map.rect.x >= 0:
             map.rect.x = -1
         if map.rect.x == -1:
-            obj.rect.x += obj.s
+            obj.rect.x += obj.rect.x
         if map.rect.x <= -4800:
             map.rect.x = -4799
         if map.rect.x == -4799:
-            obj.rect.x -= obj.s
+            obj.rect.x -= obj.rect.x
 
     def update(self, target):
         self.dx = -(target.rect.x + target.rect.w // 2 - 1600 // 2)
