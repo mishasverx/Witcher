69751125224a2debabe7dd2acbb08a702e3bfe4a
dmitr <dmitriyeltanskiy48@yandex.ru>
2022-02-25 17:59:04 +0300

    МИША ЛОООООООХ

--- camera.py
+++ camera.py
@@ -5,13 +5,13 @@ class Camera:
     def apply(self, obj, map, w):
         obj.rect.x += self.dx
         if map.rect.x >= 0:
-            map.rect.x = -1
-        if map.rect.x == -1:
-            pass
+            map.rect.x = -4800
+        if map.rect.x == -4800:
+            obj.rect.x += self.dx
         if map.rect.x <= -4800:
-            map.rect.x = -4799
-        if map.rect.x == -4799:
-            obj.rect.x -= obj.rect.x
+            map.rect.x = 0
+        if map.rect.x == 0:
+            obj.rect.x -= self.dx
 
     def update(self, target):
         self.dx = -(target.rect.x + target.rect.w // 2 - 1600 // 2)
